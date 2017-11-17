95f3e8bb928d38ac651c668035a7a9c7708cda4c
lianruinan <478383885@qq.com>
2017-11-17 09:55:01 +0800

    My firstly commit

---
+++ src/edu/scut/software/TestClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.scut.software;
+
+/**
+ * @author Aiuna
+ *
+ */
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
