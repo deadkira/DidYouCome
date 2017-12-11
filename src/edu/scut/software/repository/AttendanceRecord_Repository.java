0754e04cbfca088ef540d0b5e4d9c5d3491eef1e
lianruinan <478383885@qq.com>
2017-12-11 22:08:47 +0800

    22:08

--- src/edu/scut/software/repository/AttendanceRecord_Repository.java
+++ src/edu/scut/software/repository/AttendanceRecord_Repository.java
@@ -8,6 +8,6 @@ import edu.scut.software.entity.AttendanceRecord;
 public interface AttendanceRecord_Repository extends JpaRepository<AttendanceRecord, Integer> {
 	AttendanceRecord getByLessonIdAndStudentId(Integer lessonId, Integer studentId);
 
-	@Query(value = "select count(*) from AttendanceRecord a,Course c where a.studentId=?1 and a.lessonId in (select l.id from Lesson l where l.courseId=?2)")
+	@Query(value = "select count(*) from AttendanceRecord a where a.studentId=?1 and a.lessonId in (select l.id from Lesson l where l.courseId=?2)")
 	Integer getAttendingTimes(Integer studentId, Integer courseId);
 }
